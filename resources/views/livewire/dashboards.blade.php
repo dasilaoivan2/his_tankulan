245b02017240662496bdf712e5ff2b25163d796e
Ivan Dasilao <dasilaoivan2@gmail.com>
2023-02-13 08:55:50 +0800

    type update

--- resources/views/livewire/dashboards.blade.php
+++ resources/views/livewire/dashboards.blade.php
@@ -112,7 +112,7 @@
                                 <table class="table-auto w-full">
                                     <thead>
                                         <tr class="bg-gray-100">
-                                            <th class="px-4 py-2">Household Classification</th>
+                                            <th class="px-4 py-2">Household Type</th>
                                             <th class="px-4 py-2">No. of Households</th>
                                         </tr>
                                     </thead>
